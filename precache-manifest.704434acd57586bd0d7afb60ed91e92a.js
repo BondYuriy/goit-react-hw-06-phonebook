a2f2dca9490500535c8c3de2c9771910f21389b4
Yuriy Bondar <bondar.europe@gmail.com>
2020-01-10 18:41:01 +0200

    Updates

---
+++ precache-manifest.704434acd57586bd0d7afb60ed91e92a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a64ee8ce7c3ecd97d2aec8e5798b95",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "11b94bb128fa5414aede",
+    "url": "/goit-react-hw-06-phonebook/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "a15b56c79bf8fa73f51b",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.29b9716a.chunk.css"
+  },
+  {
+    "revision": "11b94bb128fa5414aede",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.ea2ec1ce.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.ea2ec1ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a15b56c79bf8fa73f51b",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.00ffa905.chunk.js"
+  },
+  {
+    "revision": "8ef94a824e6f3ee6d7e7",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b34c3905.js"
+  }
+]);
